826164f1abbaabfb6ae45f6f9d694a49e206e9da
mracsys <mracsys@gmail.com>
2023-07-16 15:47:55 -0400

    bk key ring rando bug

--- WorldGraph.js
+++ WorldGraph.js
@@ -228,6 +228,11 @@ class WorldGraph {
 
                     if (loc.vanilla_item.name === dungeon_text('Boss Key', dungeon)) {
                         shuffle_setting = dungeon !== 'Ganons Castle' ? world.settings.shuffle_bosskeys : world.settings.shuffle_ganon_bosskey;
+                        // OOTR bug, BKs are starting items if key rings are on,
+                        // key rings give BKs, and small keysy is on
+                        if (world.settings.key_rings.includes(dungeon) && world.settings.keyring_give_bk && world.settings.shuffle_smallkeys == 'remove') {
+                            world.state.collect(loc.vanilla_item);
+                        }
                     } else if (loc.vanilla_item.name === dungeon_text('Small Key', dungeon)) {
                         shuffle_setting = world.settings.shuffle_smallkeys;
                     } else if (loc.type === 'SilverRupee') {
