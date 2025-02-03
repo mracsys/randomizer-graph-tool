19cc6dd1986753d43d489d49312285f519bae6d1
mracsys <mracsys@gmail.com>
2025-02-03 18:09:35 -0500

    update realrob branch again

--- src/plugins/ootr-latest/OotrVersion.ts
+++ src/plugins/ootr-latest/OotrVersion.ts
@@ -40,6 +40,7 @@ class OotrVersion implements GameVersion {
         '8.2.46 Rob-124': '2530734b9102e5193d5cf2e40230b5043e15968c',
         '8.2.46 Rob-125': '09f17cc61ca3e267bc6777df2f46ce5bbd6715ea',
         '8.2.50 Rob-128': '2030053144ce99f3364244117db64ba9585a1513',
+        '8.2.52 Rob-132': '464b5184646694ec422a7dfe2a757725d73a7e1b',
     }
     static devfenhl_version_commit_ids: VersionCommitMap = {
         '8.1.45 Fenhl-3': '1f36c6b7f37951e515b94e65cbddc8cd45c3ab13',
